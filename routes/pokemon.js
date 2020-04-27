9662c164fc4840bd48ad00c273e0c20cb2ee278a
miguel <xhpedro@hotmail.com>
2020-04-27 14:43:45 -0500

    'Actividad8'

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,14 +8,14 @@ pokemon.post("/", (req, res, next) => {
 
 pokemon.get("/", async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn});
 })
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
-    (id > 0 && id < 152) ?
+    (id > 0 && id < 723) ?
     res.status(200).json(await db.query("SELECT * FROM pokemon WHERE pok_id = " + id))  :
-    res.status(404).send("Pokemon no encontrado");
+    res.status(404).json({code: 1, message: "Pokemon no encontrado"});
 });
 
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
@@ -23,7 +23,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_name = '${name.toUpperCase()}'`);
     (pkmn.length > 0) ? 
     res.status(200).send(pkmn) :
-    res.status(404).send("Pokemon no encontrado");
+    res.status(404).json({code: 1, message: "Pokemon no encontrado"});
 })
 
 module.exports = pokemon;
